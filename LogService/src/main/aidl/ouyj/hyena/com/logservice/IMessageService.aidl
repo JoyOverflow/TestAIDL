0496380311d8c59af9650f41099858a2ac8f7777
joyoverflow <joyoverflow@sina.com>
2019-10-22 23:37:37 +0800

    修改AIDL

---
+++ LogService/src/main/aidl/ouyj/hyena/com/logservice/IMessageService.aidl
@@ -0,0 +1,5 @@
+package ouyj.hyena.com.logservice;
+import ouyj.hyena.com.logservice.Message;
+interface IMessageService {
+    void log2(in Message msg);
+}
--- LogService/src/main/java/ouyj/hyena/com/logservice/MessageService.java
+++ LogService/src/main/java/ouyj/hyena/com/logservice/MessageService.java
@@ -3,14 +3,24 @@ package ouyj.hyena.com.logservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 
 public class MessageService extends Service {
+
+    public static final String TAG = "MessageService";
     public MessageService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        return new IMessageService.Stub() {
+            @Override
+            public void log2(Message msg) throws RemoteException {
+                String result=String.format("%s：%s",msg.getTag(),msg.getText());
+                Log.d(TAG , result);
+            }
+        };
     }
 }
